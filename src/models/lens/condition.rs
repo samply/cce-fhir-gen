8fd0f27b30fd76e0be3db4a30743bfa620e90be2
Manoj Waikar <mmwaikar@gmail.com>
2025-08-21 09:49:45 +0200

    Delete old Lens models.

--- src/models/lens/condition.rs
+++
@@ -1,9 +0,0 @@
-use super::enums::{ConditionType, ValueType};
-
-pub struct Condition {
-    pub key: String,
-    pub r#type: ConditionType,
-    pub system: Option<String>,
-    pub value: Option<ValueType>,
-    pub de: Option<String>
-}
